ce3911b16bdd22cfaf8b5139019196ccca82ebb7
Minh Trang <minhtrang96.it@gmail.com>
2016-06-04 05:37:40 +0700

    Thêm tính năng UploadFacebook + sửa form thông tin

---
+++ frmUploadFacebook.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Paint
+{
+    partial class frmUploadFacebook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtTenFile = new System.Windows.Forms.TextBox();
+            this.btnChonHinh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 94);
+            this.progressBar1.MarqueeAnimationSpeed = 50;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(487, 10);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(185, 51);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(104, 23);
+            this.btnSend.TabIndex = 10;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtTenFile
+            // 
+            this.txtTenFile.Enabled = false;
+            this.txtTenFile.Location = new System.Drawing.Point(24, 23);
+            this.txtTenFile.Name = "txtTenFile";
+            this.txtTenFile.Size = new System.Drawing.Size(344, 20);
+            this.txtTenFile.TabIndex = 9;
+            // 
+            // btnChonHinh
+            // 
+            this.btnChonHinh.Location = new System.Drawing.Point(374, 21);
+            this.btnChonHinh.Name = "btnChonHinh";
+            this.btnChonHinh.Size = new System.Drawing.Size(75, 23);
+            this.btnChonHinh.TabIndex = 8;
+            this.btnChonHinh.Text = "...";
+            this.btnChonHinh.UseVisualStyleBackColor = true;
+            this.btnChonHinh.Click += new System.EventHandler(this.btnChonHinh_Click);
+            // 
+            // frmUploadFacebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 104);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtTenFile);
+            this.Controls.Add(this.btnChonHinh);
+            this.Name = "frmUploadFacebook";
+            this.Text = "frmUploadFacebook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtTenFile;
+        private System.Windows.Forms.Button btnChonHinh;
+    }
+}
---
+++ frmUploadFacebook.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class frmUploadFacebook : Form
+    {
+        public frmUploadFacebook()
+        {
+            InitializeComponent();
+
+            txtTenFile.Text = strFileName;
+        }
+
+        private bool isCompleted = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string RandomString(int length = 10)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private string GetFileType(string input)
+        {
+            return "." + input.Split('.').Last();
+        }
+
+        public static string strFileName;
+
+        private void btnChonHinh_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Image File|*.jpg;*.png;*.gif";
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                // txtTenFile.Text = of.FileName;
+
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtTenFile.Text != "")
+            {
+                btnSend.Text = "Uploading...";
+                btnSend.Enabled = false;
+                progressBar1.Style = ProgressBarStyle.Marquee;
+                isCompleted = false;
+
+                System.Threading.Thread t = new System.Threading.Thread(() =>
+                {
+                    string RanFileName = RandomString() + GetFileType(txtTenFile.Text); //Random filename - tránh trùng tên
+                    string url = "http://kawaii15-vngirl.rhcloud.com/Upimg/upload.php"; //địa chỉ file PHP
+
+                    //  AppDomain.CurrentDomain.BaseDirectory: Tìm ra đường dẫn của file .exe
+                    File.Copy(txtTenFile.Text, AppDomain.CurrentDomain.BaseDirectory + "/" + RanFileName); //chép file qua thư mục tạm
+                    // Mở kết nối. 
+                    WebClient client = new WebClient();
+                    // 
+                    client.UploadFile(url, "POST", AppDomain.CurrentDomain.BaseDirectory + "/" + RanFileName); // UploadFIle
+
+                    File.Delete(AppDomain.CurrentDomain.BaseDirectory + "/" + RanFileName); // Xóa file tạm
+
+                    Process.Start("https://www.facebook.com/sharer/sharer.php?u=http://kawaii15-vngirl.rhcloud.com/Upimg/uploads/" + RanFileName); // Khởi động URL chia sẻ
+                    isCompleted = true;
+                });
+                t.Start();
+                while (isCompleted != true) // Chờ upload xong
+                {
+                    Application.DoEvents();
+                }
+                btnSend.Text = "Send";
+                btnSend.Enabled = true;
+                progressBar1.Style = ProgressBarStyle.Blocks;
+            }
+            else
+            {
+                MessageBox.Show("Choose IMG");
+            }
+        }
+    }
+}
